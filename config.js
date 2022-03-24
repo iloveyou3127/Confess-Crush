dae691838e41f3cc72e658d113930679ff2f2080
iloveyou3127 <102283619+iloveyou3127@users.noreply.github.com>
2022-03-24 17:46:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Gửi ban yeu dau >3",
-    introTitle: 'Ban yeu dau',
+    titleWeb: "chao anh",
+    introTitle: 'bbi Thien',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
+    btnIntro: 'hehe🐸',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333 eheheheh',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Ship mình chục tỉ cái đã nha roi tinh tiep.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/Ira.3127/' 
+    btnYes: 'thich lamm<3',
+    btnNo: 'khongg nhă>:(',
+    question: '....mà sao anh lai yêu em 🙁?',
+    btnReply: 'kinh gui anhh:>',
+    reply: 'iu thi iu kim chi=))))',
+    mess: 'ge vay sao oke nhe iu anhh<',
+    messDesc: ' anh to^`i vai nhma iu anhh ✨.',
+    btnAccept: 'oki nhe <33',
+    messLink: 'https://www.facebook.com/kymchie.1911' 
 }
